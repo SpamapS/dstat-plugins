0bd2fffcab47c79999e5bf20b881a69193855bd9
Clint Byrum <clint@fewbar.com>
2015-10-06 16:07:59 -0700

    Fix install script

--- dstat_plugins/__init__.py
+++ dstat_plugins/__init__.py
@@ -1,9 +1,20 @@
+import glob
 import shutil
 import sys
+import os
+import os.path
 
 import pkg_resources as pr
 
 def install():
     destdir = sys.argv[1]
-    datadir = pr.resource_filename(__name__, 'plugins/dstat_mysql5_innodb.py')
-    shutil.copytree(datadir, destdir)
+    datadir = pr.resource_filename('dstat_plugins', 'plugins')
+    try:
+        os.makedirs(destdir)
+    except OSError:
+        if not os.path.isdir(destdir):
+            sys.stderr.write("{} could not be created and does not "
+                             "exist.\n".format(destdir))
+            sys.exit(1)
+    for plugin in glob.glob(os.path.join(datadir, 'dstat_*')):
+        shutil.copy(plugin, destdir)
